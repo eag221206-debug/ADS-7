db370da551b750523c7899c3ab2123f9ac4b9d04
eag221206-debug <eag221206@gmail.com>
2026-05-19 19:26:17 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,13 +1,24 @@
+// Copyright 2021 NNTU-CS
 #include "train.h"
 
 Train::Train() : countOp(0), first(nullptr) {}
 
+Train::~Train() {
+    if (!first) {
+        return;
+    }
+    Car* current = first->next;
+    while (current != first) {
+        Car* to_delete = current;
+        current = current->next;
+        delete to_delete;
+    }
+    delete first;
+}
+
 void Train::addCar(bool light) {
-    Car* newCar = new Car;
-    newCar->light = light;
-    newCar->next = nullptr;
-    newCar->prev = nullptr;
-    if (first == nullptr) {
+    Car* newCar = new Car{light, nullptr, nullptr};
+    if (!first) {
         first = newCar;
         first->next = first;
         first->prev = first;
@@ -20,56 +31,47 @@ void Train::addCar(bool light) {
     }
 }
 
-int Train::getOpCount() {
-    return countOp;
-}
-
 int Train::getLength() {
-    if (first == nullptr) return 0;
     countOp = 0;
-    
-    if (!first->light) {
-        int len = 1;
-        Car* current = first->next;
-        countOp++;
-        while (current != first) {
-            len++;
-            current = current->next;
+    if (!first || first->next == first) {
+        return first ? 1 : 0;
+    }
+
+    Car* base = first;
+    base->light = !base->light;
+
+    for (int d = 1; ; ++d) {
+        Car* probe = base;
+        for (int i = 0; i < d; ++i) {
+            probe = probe->next;
             countOp++;
         }
-        return len;
-    }
-    
-    first->light = false;
-    int len = 1;
-    Car* current = first->next;
-    countOp++;
-    
-    while (current->light) {
-        current->light = false;
-        len++;
-        current = current->next;
-        countOp++;
-    }
-    
-    if (current == first) {
-        return len;
-    }
-    
-    current->light = false;
-    Car* marker = current;
-    int count = 1;
-    current = current->next;
-    countOp++;
-    
-    while (current != marker) {
-        count++;
-        if (current->light) {
-            current->light = false;
+
+        probe->light = !probe->light;
+
+        for (int i = 0; i < d; ++i) {
+            probe = probe->prev;
+            countOp++;
         }
-        current = current->next;
-        countOp++;
+
+        if (base->light != probe->light) {
+             return d;
+        }
+
+        for (int i = 0; i < d; ++i) {
+            probe = probe->next;
+            countOp++;
+        }
+        probe->light = !probe->light;
+
+        for (int i = 0; i < d; ++i) {
+            probe = probe->prev;
+            countOp++;
+        }
     }
-    
-    return count;
+    return -1;
+}
+
+int Train::getOpCount() {
+    return countOp;
 }
