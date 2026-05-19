e632a55f2d23156516e56004bb9c551408f7037b
eag221206-debug <eag221206@gmail.com>
2026-05-19 19:03:17 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,9 +1,14 @@
 // Copyright 2022 NNTU-CS
 #include "train.h"
+
 Train::Train() : countOp(0), first(nullptr) {}
+
 void Train::addCar(bool light) {
-    Car* newCar = new Car{light, nullptr, nullptr};
-    if (!first) {
+    Car* newCar = new Car;
+    newCar->light = light;
+    newCar->next = nullptr;
+    newCar->prev = nullptr;
+    if (first == nullptr) {
         first = newCar;
         first->next = first;
         first->prev = first;
@@ -15,43 +20,36 @@ void Train::addCar(bool light) {
         first->prev = newCar;
     }
 }
-int Train::getLength() {
-    countOp = 0;
-    if (!first) {
-        return 0;
-    }
-    if (first->next == first) {
-        return 1;
-    }
-    Car* base = first;
-    base->light = !base->light;
-    for (int d = 1; ; ++d) {
-        Car* probe = base;
-        for (int i = 0; i < d; ++i) {
-            probe = probe->next;
-            countOp++;
-        }
-        probe->light = !probe->light;
-        for (int i = 0; i < d; ++i) {
-            probe = probe->prev;
-            countOp++;
-        }
-        if (base->light != probe->light) {
-            probe->light = !probe->light;
-            return d;
-        }
-        for (int i = 0; i < d; ++i) {
-            probe = probe->next;
-            countOp++;
-        }
-        probe->light = !probe->light;
-        for (int i = 0; i < d; ++i) {
-            probe = probe->prev;
-            countOp++;
-        }
-    }
-    return -1;
-}
+
 int Train::getOpCount() {
     return countOp;
+}
+
+int Train::getLength() {
+    if (first == nullptr) return 0;
+    countOp = 0;
+    first->light = false;
+    Car* current = first;
+    int length = 1;
+    while (true) {
+        for (int i = 0; i < length; ++i) {
+            current = current->next;
+            countOp++;
+        }
+        if (!current->light) {
+            for (int i = 0; i < length; ++i) {
+                current = current->prev;
+                countOp++;
+            }
+            if (!current->light && current == first) {
+                return length;
+            }
+            length++;
+            current = current->next;
+            countOp++;
+        } else {
+            current->light = false;
+            length++;
+        }
+    }
 }
